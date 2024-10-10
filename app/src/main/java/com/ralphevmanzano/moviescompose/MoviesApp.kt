941df7a6d09635daf969cb6644d74680166801e9
Ralph Manzano <ralph.manzano@spectrum.global>
2024-10-10 14:34:51 +0800

    Add Shakebugs for bug reporter and logger

--- app/src/main/java/com/ralphevmanzano/moviescompose/MoviesApp.kt
+++ app/src/main/java/com/ralphevmanzano/moviescompose/MoviesApp.kt
@@ -1,7 +1,16 @@
 package com.ralphevmanzano.moviescompose
 
 import android.app.Application
+import com.shakebugs.shake.Shake
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MoviesApp: Application()
+class MoviesApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Shake.getReportConfiguration().isConsoleLogsEnabled = false
+        Shake.start(this, BuildConfig.SHAKE_API_KEY)
+    }
+}
